0b0852df571380a739ffaa2822419dbaaff7f360
javie <javie@Portatil-Si4GrC>
2026-02-16 12:58:22 +0100

    Commit 1 Ejercicio 2

--- src/main/java/giis/demo/jdbc/Entity.java
+++ src/main/java/giis/demo/jdbc/Entity.java
@@ -15,4 +15,5 @@ public class Entity {
 	// https://www.sitepoint.com/declutter-pojos-with-lombok-tutorial/
 	// http://www.baeldung.com/intro-to-project-lombok
 	// Prueba PA Ejercicio 1
+	//Prueba PA Ejercicio 2 rama 2
 }
